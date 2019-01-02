30857fc52bbaba06f554169ca6af8e2e62fa4da3
reddart <yasir.f93@gmail.com>
2019-01-02 22:14:00 +0530

    bangalore craiglist scraper

---
+++ craiglist/spiders/jobs.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy import Request
+
+
+class JobsSpider(scrapy.Spider):
+    name = 'jobs'
+    allowed_domains = []
+    start_urls = ['http://bangalore.craigslist.org/search/jjj/']
+
+    def parse(self, response):
+        jobs=response.xpath('.//*[@class="result-row"]')
+        for job in jobs:
+           # title=job.xpath('.//p/a/text()').extract_first()
+           # address=job.xpath('.//*[@class="result-hood"]/text()').extract_first()
+            link=job.xpath('.//p/a/@href').extract_first()
+            yield Request(link,callback=self.parse_job,meta={'URL':link})
+        relative_next_url=response.xpath('.//*[@class="button next"]/@href').extract_first()
+        absolute_next_url=response.urljoin(relative_next_url)
+        yield Request(absolute_next_url,callback=self.parse)
+    def parse_job(self,response):
+        title=response.xpath('.//*[@id="titletextonly"]/text()').extract()
+        address=response.xpath('.//*[@class="postingtitletext"]/small/text()').extract_first()
+        lines=response.xpath('.//*[@id="postingbody"]/text()').extract()
+        description="".join(lines)
+        compensation=response.xpath('.//*[@class="attrgroup"]/span[1]/b/text()').extract()
+        emptype=response.xpath('.//*[@class="attrgroup"]/span[2]/b/text()').extract()
+        yield{
+                'Title':title,
+                'location':address,
+                'Job Description':description,
+                'Compensation':compensation,
+                'Employment Type':emptype,
+                'URL': response.meta.get('URL')
+            }
