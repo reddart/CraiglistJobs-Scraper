a48c8069f6a878822958c656914a7313669d4243
reddart <44072689+reddart@users.noreply.github.com>
2019-01-02 22:16:06 +0530

    Update jobs.py

--- craiglist/spiders/jobs.py
+++ craiglist/spiders/jobs.py
@@ -11,13 +11,14 @@ class JobsSpider(scrapy.Spider):
     def parse(self, response):
         jobs=response.xpath('.//*[@class="result-row"]')
         for job in jobs:
-           # title=job.xpath('.//p/a/text()').extract_first()
-           # address=job.xpath('.//*[@class="result-hood"]/text()').extract_first()
             link=job.xpath('.//p/a/@href').extract_first()
             yield Request(link,callback=self.parse_job,meta={'URL':link})
+            
         relative_next_url=response.xpath('.//*[@class="button next"]/@href').extract_first()
         absolute_next_url=response.urljoin(relative_next_url)
+        
         yield Request(absolute_next_url,callback=self.parse)
+        
     def parse_job(self,response):
         title=response.xpath('.//*[@id="titletextonly"]/text()').extract()
         address=response.xpath('.//*[@class="postingtitletext"]/small/text()').extract_first()
@@ -25,6 +26,7 @@ class JobsSpider(scrapy.Spider):
         description="".join(lines)
         compensation=response.xpath('.//*[@class="attrgroup"]/span[1]/b/text()').extract()
         emptype=response.xpath('.//*[@class="attrgroup"]/span[2]/b/text()').extract()
+        
         yield{
                 'Title':title,
                 'location':address,
